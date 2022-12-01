2ed2019e54a2dc44b087c034bca7cb3f5b0672f6
Max <maximilian.franschitz@gmx.at>
2022-12-01 18:50:39 +0100

    added solutions in R

---
+++ Day01/part2.r
@@ -0,0 +1,15 @@
+mydata <- scan(file = "./Day01/input.txt", blank.lines.skip = FALSE)
+
+current_sum <- 0
+sums_vector <- c()
+
+for (element in mydata) {
+    if (!is.na(element)) {
+        current_sum <- current_sum + element
+    } else {
+        sums_vector <- append(sums_vector, current_sum)
+        current_sum <- 0
+    }
+}
+
+print(sum(sort(sums_vector, decreasing = TRUE)[1:3]))
