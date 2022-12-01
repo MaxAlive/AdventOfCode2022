fbea592296de90183c170b7a208599c3ae264713
Max <maximilian.franschitz@gmx.at>
2022-12-01 08:23:33 +0100

    added python solutions and input for day 1

--- Day01/part2.py
+++ Day01/part2.py
@@ -2,11 +2,10 @@ with open('Day01/input.txt') as f:
     data = [value.rstrip() for value in f.readlines()]
 
 data = [int(value) if value else "" for value in data] + [""]
-max_calories = 0
+sums_of_calories = []
 
 while data:
-    if (calories := sum(data[:data.index('')])) > max_calories:
-        max_calories = calories
+    sums_of_calories.append(sum(data[:data.index('')]))
     data = data[data.index('')+1:]
 
-print(max_calories)
+print(sum(sorted(sums_of_calories)[-3:]))
